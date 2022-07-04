8647a253b81de5a7ba053396efb53ff2a6bb56f3
lorenamucheniski <lomucheniski25@gmail.com>
2022-07-04 11:33:00 -0300

    conversor de medidas

---
+++ ex06.py
@@ -0,0 +1,5 @@
+print('========== CONVERSOR DE MEDIDAS ==========')
+mt = float(input('Qual o valor, em metros, a ser convertido?'))
+c = mt * 100
+mi = mt * 1000
+print('O valor de {} metros é igual a {} centimetros e {} milímetros'.format(mt, c, mi))
