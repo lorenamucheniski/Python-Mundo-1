9ca7294b95828176a6d13530fa00cd1bd694bfdc
lorenamucheniski <lomucheniski25@gmail.com>
2022-07-04 11:20:14 -0300

    calculo media

---
+++ ex05.py
@@ -0,0 +1,5 @@
+n = str(input('Qual o nome do aluno(a)?'))
+n1 = float(input('Qual a 1° nota do aluno(a)?'))
+n2 = float(input('Qual a 2° nota do aluno?'))
+media = (n1 + n2)/2
+print('A média do aluno(a) {} é de {}'.format(n, media))
