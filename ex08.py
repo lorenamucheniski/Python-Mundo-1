d3cd88c0f413055d0b780056ebbd2a002e45514b
lorenamucheniski <lomucheniski25@gmail.com>
2022-07-06 10:31:47 -0300

    conversor de moedas

---
+++ ex08.py
@@ -0,0 +1,4 @@
+print('====== CONVERSOR DE MOEDAS ======')
+rs = float(input('Quantos reais você tem?'))
+us = rs/5.41
+print('Com {:.2f} reais você consegue comprar {:.2f} dólares.'.format(rs, us))
