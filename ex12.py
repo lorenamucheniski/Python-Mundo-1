4c6db3967825a88e65ab85035b691b6dd97030c0
lorenamucheniski <lomucheniski25@gmail.com>
2022-07-13 15:20:15 -0300

    quebrando um número

---
+++ ex12.py
@@ -0,0 +1,4 @@
+from math import trunc
+print('=========== QUEBRANDO UM NÚMERO ==========')
+num = float(input('Digite um valor real: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
